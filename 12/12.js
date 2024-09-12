81f12c5263261af6464fa77f3fbceea4fd3a69bd
Yejin324 <cheonyj96@gmail.com>
2024-09-12 11:50:45 +0900

    JS240912

--- 12/12.js
+++ 12/12.js
@@ -1,7 +1,80 @@
+//전역변수
+const testAPI='82ca741a2844c5c180a208137bb92bd7';
+
+const getDetail = (movieCd) => {
+  // alert(movieCd);
+
+  //지역변수
+  let urlMovie = `https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?`;
+  
+  const footer = document.querySelector('#mvinfo');
+
+  urlMovie = `${urlMovie}key=${testAPI}&movieCd=${movieCd}`;
+
+  console.log(urlMovie);
+  fetch(urlMovie)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data.movieInfoResult.movieInfo)
+
+      let movieNm = data.movieInfoResult.movieInfo.movieNm;
+      let movieNmEn = data.movieInfoResult.movieInfo.movieNmEn;
+      let actors = data.movieInfoResult.movieInfo.actors;
+      let directors = data.movieInfoResult.movieInfo.directors;
+      let genres = data.movieInfoResult.movieInfo.genres;
+      let companys = data.movieInfoResult.movieInfo.companys;
+
+      let openDtYear = data.movieInfoResult.movieInfo.openDt.slice(0,4);
+      let openDtMonth = data.movieInfoResult.movieInfo.openDt.slice(4,6);
+      let openDtDay = data.movieInfoResult.movieInfo.openDt.slice(6,8);
+      let openDt = `${openDtYear}년 ${openDtMonth}월 ${openDtDay}일`;
+      
+      let actorstm = actors.slice(0,3).map((item, index) => `${item.peopleNm}${index<actors.slice(0,3).length-1?"/":""}`);
+      let directortm = directors.slice(0,3).map((item, index) => `${item.peopleNm}${index<directors.slice(0,3).length-1?"/":""}`);
+      let companystm = companys.slice(0,1).map((item, index) => `${item.companyNm}${index<companys.slice(0,1).length-1?"/":""}`);
+      let genrestm = genres.map(item => `${item.genreNm}`);
+
+      let actorStringLength = actorstm.join(' / ').length;
+      let minLineLength = 20; // 구분선의 최소 길이 설정
+      let lineLength = Math.max(actorStringLength, minLineLength);// 계산된 길이와 최소 길이 중 큰 값을 선택
+      let lineString = '─'.repeat(lineLength-3);
+
+      footer.innerHTML = 
+      ` <div id="mvinfoName">
+        <h2 id='movieNm'>${movieNm}</h2>
+        <span>${movieNmEn}</span>
+        </div>
+        <div id="mvinfoActor">
+          <div class='tt'>출&nbsp;&nbsp;&nbsp;연</div>
+          ${actorstm.join(' ')}
+        </div>
+        <div id='line'>
+         ${lineString}
+        </div>
+        <div id="mvinfoDirector">
+          <div class='tt'>감&nbsp;&nbsp;&nbsp;독</div>
+          ${directortm.join(' ')}
+        </div>
+        <div id="mvinfoGenre">
+          <div class='tt'>장&nbsp;&nbsp;&nbsp;르</div>
+          ${genrestm.join(' ')}
+        </div>
+        <div id="mvinfoOpenDt">
+          <div class='tt'>개봉일</div>
+          ${openDt}
+        </div>
+        <div id="mvinfoCompanyNm">
+          <div class='tt'>배&nbsp;&nbsp;&nbsp;급</div>
+          ${companystm.join(' ')}
+        </div>`; 
+    })
+    .catch(err => console.error(err));
+}
+
 //OPEN API 데이터 가져오기
 const getData = (setDt, ul, rdo1V)=>{
   console.log(rdo1V);
-  const testAPI = '82ca741a2844c5c180a208137bb92bd7';
+  
   let url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
   url = `${url}key=${testAPI}&targetDt=${setDt}`;
 
@@ -16,14 +89,16 @@ const getData = (setDt, ul, rdo1V)=>{
       let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
 
       let tm = dailyBoxOfficeList.map(item => 
-        `<li class="mvli">
-          <span class="rank">${item.rank}</span>
-          <span class="movieNm">${item.movieNm}</span>
-          <span class="openDt">${item.openDt}</span>
-          <span class="rankInten" id="${item.rankInten == 0 ? "black" : (item.rankInten > 0 ? "blue" : "red")}">
-          ${item.rankInten == 0 ? "-  " : (item.rankInten > 0 ? "▲" : "▼")} 
-          ${item.rankInten != 0 ? Math.abs(item.rankInten) : ''}</span>
-        </li>`) 
+        `<a href="#" onClick="getDetail(${item.movieCd})">
+          <li class="mvli">
+            <span class="rank">${item.rank}</span>
+            <span class="movieNm">${item.movieNm}</span>
+            <span class="openDt">${item.openDt}</span>
+            <span class="rankInten" id="${item.rankInten == 0 ? "black" : (item.rankInten > 0 ? "blue" : "red")}">
+            ${item.rankInten == 0 ? "-  " : (item.rankInten > 0 ? "▲" : "▼")} 
+            ${item.rankInten != 0 ? Math.abs(item.rankInten) : ''}</span>
+          </li>
+        </a>`) 
       ul.innerHTML = tm.join(''); 
     })
     .catch(err => console.error(err));
@@ -52,25 +127,31 @@ const getYesterday= () => {
 
 const getRdo = () => {
   //radio 요소 가져오기
-  const rdoT = document.querySelector('#rdoT');
-  const rdoK = document.querySelector('#rdoK');
-  const rdoF = document.querySelector('#rdoF');
+  // const rdoT = document.querySelector('#rdoT');
+  // const rdoK = document.querySelector('#rdoK');
+  // const rdoF = document.querySelector('#rdoF');
 
-  console.log(rdoT.checked);
-  console.log(rdoK.checked);
-  console.log(rdoF.checked);
+  // console.log(rdoT.checked);
+  // console.log(rdoK.checked);
+  // console.log(rdoF.checked);
 
-  if(rdoT.checked) return rdoT.value;
-  else if (rdoK.checked) return rdoK.value;
-  else return rdoF.value;
+  // if(rdoT.checked) return rdoT.value;
+  // else if (rdoK.checked) return rdoK.value;
+  // else return rdoF.value;
+
+  //radio 버튼의 클릭된 것만 가져오기
+  const rdo = document.querySelector('input[name=rdo]:checked');
+  console.log(rdo.value);
+  return rdo.value;
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
   const dt = document.querySelector('#dt');
   const ul = document.querySelector('.sect ul');
   // const sel1 = document.querySelector('#selmovie');
-  const radios = document.querySelectorAll('input[type=radio]');
-  
+  // const radios = document.querySelectorAll('input[type=radio]');
+  // const radios = document.getElementsByName('rdo');
+  const radios = document.querySelectorAll('input[name=rdo]');
 
   let yesterday = getYesterday();
   // console.log(yesterday);  // console.log(getYesterday()); 위의 결과와 같음
